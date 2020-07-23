76c5165161053d32513032483cae0e7d23877fb1
Sara Munir <saramunir011@gmail.com>
2020-07-23 14:11:24 -0400

    setting up backend

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
+require('dotenv').config(); // --> process.env
+
 const express = require( 'express' );
+
+const orm = require( './db/orm.mongoose' );
+const PORT = process.env.PORT || 8080;
+const app = express();
+var server = app.listen( PORT, function(){ console.log( `[The Habbit], http://localhost:${PORT}` ); });
